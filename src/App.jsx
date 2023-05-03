9d326b9c22a4963617ca236cd9545cbb471a4076
yunshangzhou <yunshagzhou@163.com>
2023-05-03 18:05:51 +0800

    -eslint config acompolete

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import React, { Component } from "react";
 class App extends Component {
   render() {
     return (
-      <div className='box'>
+      <div className="box">
         <h2>我是yunshangzhou</h2>
-        <div>App 页面</div>;
+        <div>App 页面</div>
       </div>
-    )
+    );
   }
 }
 
