140c7473add3da22dfc94d8b99a6cbe189c58cc1
chenkaibang <wb.chenkaibang01@mesg.corp.netease.com>
2023-06-21 19:27:00 +0800

    feat: example添加useSize

--- src/pages/example/index.tsx
+++ src/pages/example/index.tsx
@@ -1,12 +1,15 @@
-import React, { useEffect, useRef } from "react";
-import classNames from "classnames/bind";
-import PlayerBar, { requiredDefaultProps } from "../../components/PlayerBar";
-import styles from "./index.module.css";
+import React, { useRef } from 'react';
+import classNames from 'classnames/bind';
+import { useSize } from 'ahooks';
+import PlayerBar, { requiredDefaultProps } from '../../components/PlayerBar';
+import styles from './index.module.css';
 
-import { useSize } from "ahooks";
 
 const cx = classNames.bind(styles);
 
+/**
+ * 不要给包裹控制器的父元素添加padding, useSize计算的宽度会一并带上padding值。
+ */
 const ExamplePage = () => {
   const containerRef = useRef<HTMLDivElement>(null);
   const size = useSize(containerRef);
@@ -14,14 +17,10 @@ const ExamplePage = () => {
   return (
     <div
       ref={containerRef}
-      className={cx("example-page")}
-      style={{ display: "flex", flexDirection: "column", gap: 50 }}
-    >
+      className={cx('example-page')}
+      style={{ display: 'flex', flexDirection: 'column', gap: 50 }}>
       <span>{size?.width}</span>
-      <PlayerBar
-        parentWidth={size?.width as number}
-        {...requiredDefaultProps}
-      />
+      <PlayerBar parentWidth={size?.width as number} {...requiredDefaultProps} />
       {/* <PlayerBar
         parentWidth={size?.width as number}
         {...requiredDefaultProps}
